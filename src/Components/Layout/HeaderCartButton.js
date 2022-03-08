475c10b74030f0fa5e51f0f64692b851155e171c
unknown <hassanzoro21@gmail.com>
2022-03-09 01:02:38 +0200

    adding bump animation to cart button on add/remove  cart meal items

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,11 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../UI/CartIcon";
 import cartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const ctx = useContext(cartContext);
-
+  const { meals } = ctx;
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+  useEffect(() => {
+    if (meals.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [meals]);
   const totalMeals = ctx.meals.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
@@ -15,7 +29,7 @@ const HeaderCartButton = (props) => {
   };
   return (
     <form>
-      <button className={styles.button} onClick={cartClickHandler}>
+      <button className={btnClasses} onClick={cartClickHandler}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
