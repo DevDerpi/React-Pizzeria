7950f3832a99ac90a09ce5f8e6e6402034b10bec
unknown <hassanzoro21@gmail.com>
2022-03-22 22:42:20 +0200

    creating logic of useInput hook

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,4 +1,27 @@
 import { useState } from "react";
 
-const useInput = () => {};
+const useInput = (validate) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  const valueValidity = validate(enteredValue);
+  const inputIsInvalid = !valueValidity && isTouched;
+  const inputChangeHandler = (event) => {
+    setIsTouched(true);
+    setEnteredValue(event.target.value);
+  };
+  const blurChangeHandler = () => {
+    setIsTouched(true);
+  };
+  const clearFn = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+  return {
+    enteredValue,
+    inputIsInvalid,
+    inputChangeHandler,
+    blurChangeHandler,
+    clearFn,
+  };
+};
 export default useInput;
